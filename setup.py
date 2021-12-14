b84fedb8e716bcecb32dbc3444b59da848e9f6d5
shc443 <shc443@nyu.edu>
2021-12-14 22:38:24 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='data-sema',
+    version='1.0',
+    description='SEMA VOC Multiclass Classification',
+    author='Kevin Choi',
+    author_email='kchoi@xinapse.ai',
+    license='Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)',
+    packages=find_packages(),
+    )
